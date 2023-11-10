923a41483894e8f3da251501d0b209535047cb70
thanhphuongIT <thanhphuong468@gmail.com>
2023-11-10 11:15:55 +0700

    init

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=phuong0963530072
+export POSTGRES_HOST=postgres.c2nr7pmzzvh5.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagram-phuongbt10
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
